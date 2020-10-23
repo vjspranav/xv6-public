11328e89ff876aa83b22039035edbb21d80848e2
vjspranav <pranavasri@live.in>
2020-10-23 22:23:04 +0530

    Implement waitx

--- defs.h
+++ defs.h
@@ -118,6 +118,7 @@ void            setproc(struct proc*);
 void            sleep(void*, struct spinlock*);
 void            userinit(void);
 int             wait(void);
+int             waitx(int *wtime, int *rtime);
 void            wakeup(void*);
 void            yield(void);
 void            updateRuntime(void);
--- proc.h
+++ proc.h
@@ -49,9 +49,9 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
-  uint ctime;		       // Process creation time
-  uint etime;		       // Process end time
-  uint rtime;		       // Process run time
+  int ctime;		       // Process creation time
+  int etime;		       // Process end time
+  int rtime;		       // Process run time
 };
 
 // Process memory is laid out contiguously, low addresses first:
